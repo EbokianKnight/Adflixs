c54eaafc87707d030ac44b7f56c31578a2e7c8da
Adam <ebokianknight@gmail.com>
2016-03-29 14:15:10 -0400

    Setup ReactDOM React Router

---
+++ frontend/components/homepage.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var HomePage = React.createClass({
+
+	render: function() {
+		return (
+			<div>HomePage Understands</div>
+		);
+	}
+
+});
+
+module.exports = HomePage;
---
+++ frontend/root.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var IndexRoute = require('react-router').IndexRoute;
+
+var App = require('./components/app');
+var HomePage = require('./components/homepage');
+
+var routes = (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage}/>
+  </Route>
+);
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  ReactDOM.render(
+    <Router>{ routes }</Router>,
+    document.getElementById('root')
+  );
+});
