1f5c1f481b2361f53bfa7960291ddf45efe926d5
Adam <ebokianknight@gmail.com>
2016-03-30 11:13:14 -0400

    Ad/Genre Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
 	resources :users, only: [:new, :create]
 
+	namespace :api, defaults: { format: :json } do
+		resources :genres, only: [:show, :index, :create, :destroy]
+		resources :ads, only: [:show, :index, :create, :destroy]
+	end
+
 	root to: "static_pages#root"
   get '*unmatched_route', to: 'static_pages#root'
 end
