0191c3fd0fa6a628288457ec9d95e8eb54afb992
Adam <ebokianknight@gmail.com>
2016-03-30 17:09:40 -0400

    Genre Actions Debugged

--- frontend/actions/genre_actions.js
+++ frontend/actions/genre_actions.js
@@ -2,7 +2,7 @@ var Dispatcher = require('../dispatcher/dispatcher.js');
 var GenreConstants = require('../constants/genre_constants.js');
 
 module.exports = {
-  receiveAllGenres: function (genres) {
+  recieveAllGenres: function (genres) {
     Dispatcher.dispatch({
       actionType: GenreConstants.GENRES_RECEIVED,
       genres: genres
