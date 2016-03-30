8f4776d455f92c68ce0e98fc4ae3cf31775f078f
Adam <ebokianknight@gmail.com>
2016-03-30 11:06:59 -0400

    Models Associated

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,4 +1,5 @@
 class Ad < ActiveRecord::Base
 	validates :company, :product, :description, :year, presence: true
 	has_many :ad_genres
+	has_many :genres, through: :ad_genres, source: :genre
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,5 @@
 class Genre < ActiveRecord::Base
 	validates :name, uniqueness: true
 	has_many :ad_genres
+	has_many :ads, through: :ad_genres, source: :ad
 end
