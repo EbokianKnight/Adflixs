93124d76f1757f370cb1eaf804c3bbae7c4e4f5f
Adam <ebokianknight@gmail.com>
2016-03-31 16:53:54 -0400

    Create AdDetail Pane Component

--- frontend/components/ads/ad_detail_pane.jsx
+++ frontend/components/ads/ad_detail_pane.jsx
@@ -1,16 +1,23 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var AdAction = require('../../actions/ad_actions');
+
 
 var AdDetailPane = React.createClass({
 
+	close_details: function () {
+		AdAction.closeDetails();
+	},
+
 	render: function() {
 		return (
-			<div className="ad-detail-pane">
-				STUFF
+			<div className="ad-detail-pane-box">
+				<div className="ad-detail-pane">
+					<button className="top-right-x" onClick={this.close_details}>X</button>
+				</div>
 			</div>
 		);
 	}
-
 });
 
 module.exports = AdDetailPane;
