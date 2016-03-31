759befa3b63dab5b2f5087b14c8f9e2427d46759
Adam <ebokianknight@gmail.com>
2016-03-31 16:48:45 -0400

    Fixed Typo

--- frontend/actions/ad_actions.js
+++ frontend/actions/ad_actions.js
@@ -2,17 +2,23 @@ var Dispatcher = require('../dispatcher/dispatcher.js');
 var AdConstants = require('../constants/ad_constants.js');
 
 module.exports = {
-  receiveAllAdverts: function (adverts) {
+  recieveAllAdverts: function (adverts) {
     Dispatcher.dispatch({
       actionType: AdConstants.ADS_RECEIVED,
       adverts: adverts
     });
   },
 
-  receiveSingleAdverts: function (advert) {
+	recieveAdvert: function (advert) {
     Dispatcher.dispatch({
       actionType: AdConstants.AD_RECEIVED,
       advert: advert
     });
-  }
+  },
+
+	closeDetails: function () {
+		Dispatcher.dispatch({
+      actionType: AdConstants.CLOSE_DETAILS,
+    });
+	}
 };
