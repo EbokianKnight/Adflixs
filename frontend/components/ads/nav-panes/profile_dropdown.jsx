5490cf8202cd701eb5ebe664eba81b2c7d844500
Adam <ebokianknight@gmail.com>
2016-04-01 17:25:40 -0400

    Make Logout Functionality

--- frontend/components/ads/nav-panes/profile_dropdown.jsx
+++ frontend/components/ads/nav-panes/profile_dropdown.jsx
@@ -2,8 +2,10 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var SessionStore = require('../../../stores/session_store');
 var ReactCSS = require('react-addons-css-transition-group');
+var UserUtil = require('../../../util/user_util');
 
 var ProfileDropDown = React.createClass({
+  contextTypes: { router: PropTypes.object.isRequired },
 
   getInitialState: function() {
     return {
@@ -19,16 +21,32 @@ var ProfileDropDown = React.createClass({
     this.setState({ show: false });
   },
 
+  goToLogout: function (e) {
+    UserUtil.logout(this.redirectToHome);
+  },
+
+  redirectToHome: function () {
+    this.context.router.push("/")
+  },
+
+  goToAccount: function (e) {
+    console.log("We Link to the Account Pages!");
+  },
+
   createRollOverMenu: function () {
     if (this.state.show) {
       return (
-        <div className="nav-profile-dropdown" onMouseLeave={this.rollBack} key="1">
+        <div className="nav-profile-dropdown"
+          onMouseLeave={this.rollBack} key="1">
           <spacer className="sixty-pix">
             <div className="nav-white-arrow"/>
           </spacer>
           <div className="nav-white-bar"/>
-          <spacer className="nav-menu-fill">
-
+          <spacer className="nav-menu-fill group">
+            <button className="nav-profile-item"
+              onClick={this.goToAccount}>Your Account</button>
+            <button className="nav-profile-item"
+              onClick={this.goToLogout}>Logout of Adflix</button>
           </spacer>
         </div>
       );
