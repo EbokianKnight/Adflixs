7952be254051197364e90ab5a5c6564f40d99a5d
Adam <ebokianknight@gmail.com>
2016-03-31 20:17:42 -0400

    refactored css ad-display-features

--- frontend/components/ads/ad_detail_pane.jsx
+++ frontend/components/ads/ad_detail_pane.jsx
@@ -5,6 +5,22 @@ var AdAction = require('../../actions/ad_actions');
 
 var AdDetailPane = React.createClass({
 
+	createOverview: function () {
+		return (
+			<div className="ad-display-container">
+				<h2 className="feature-title">The Title</h2>
+				<ul className="feature-info-bar group">
+					<li>&#x2605; &#x2605; &#x2605; &#x2605; &#x2605;</li>
+					<li>Company</li>
+					<li>Product</li>
+					<li>Year</li>
+				</ul>
+				<p className="feature-description"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<button className="more-features">Add To MyList</button>
+			</div>
+		);
+	},
+
 	close_details: function () {
 		AdAction.closeDetails();
 	},
@@ -13,9 +29,19 @@ var AdDetailPane = React.createClass({
 		return (
 			<div className="ad-detail-pane-box">
 				<div className="ad-detail-pane">
-					<div className="back-grade"></div>
+					<div className="back-grade">
+						<div className="header-spacer header-left-arrow"/>
+						{ this.createOverview() }
+					</div>
+					<nav className="ad-display-nav group">
+						<button>Overview</button>
+						<button>More Like this</button>
+						<button>Details</button>
+					</nav>
 					<button className="top-right-x" onClick={this.close_details}>X</button>
+					<div className="header-spacer header-right-arrow"/>
 				</div>
+				<div className="bottom-grade"></div>
 			</div>
 		);
 	}
--- frontend/components/ads/main_header.jsx
+++ frontend/components/ads/main_header.jsx
@@ -3,25 +3,35 @@ var PropTypes = React.PropTypes;
 
 var MainHeader = React.createClass({
 
+	createOverview: function () {
+		return (
+			<div className="ad-display-container">
+				<h2 className="feature-title">The Title</h2>
+				<ul className="feature-info-bar group">
+					<li>&#x2605; &#x2605; &#x2605; &#x2605; &#x2605;</li>
+					<li>Company</li>
+					<li>Product</li>
+					<li>Year</li>
+				</ul>
+				<p className="feature-description"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<button className="more-features">Add To MyList</button>
+			</div>
+		);
+	},
+
 	render: function() {
 		return (
 			<div className="main-index-header">
+				<div className="bottom-grade"></div>
 				<div className="back-grade">
-					<button className="header-left-arrow"></button>
-					<div className="header-info-container">
-						<h1 className="feature-title">The Title</h1>
-						<ul className="feature-info-bar group">
-							<li>Company</li>
-							<li>Product</li>
-							<li>Year</li>
-						</ul>
-						<p className="feature-description"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						<div className="more-features">
-							<button>&#x2605; &#x2605; &#x2605; &#x2605; &#x2605;</button>
-							<button>Add To MyList</button>
-						</div>
-					</div>
-					<button className="header-right-arrow"></button>
+					<div className="header-spacer header-left-arrow"/>
+					{ this.createOverview() }
+					<nav className="ad-display-nav group top">
+						<button>Overview</button>
+						<button>More Like this</button>
+						<button>Details</button>
+					</nav>
+					<button className="header-right-arrow header-spacer"></button>
 				</div>
 			</div>
 		);
