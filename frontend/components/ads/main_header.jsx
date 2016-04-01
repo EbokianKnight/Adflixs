0b4b2c6897de527268337de365324582c0ecb9c8
Adam <ebokianknight@gmail.com>
2016-03-31 23:11:19 -0400

    Header / AdDetail factored to share components

--- frontend/components/ads/main_header.jsx
+++ frontend/components/ads/main_header.jsx
@@ -1,21 +1,16 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var OverviewDetail = require('./feature_panes/overview');
 
 var MainHeader = React.createClass({
 
-	createOverview: function () {
+	createMenuButtons: function () {
 		return (
-			<div className="ad-display-container">
-				<h2 className="feature-title">The Title</h2>
-				<ul className="feature-info-bar group">
-					<li>&#x2605; &#x2605; &#x2605; &#x2605; &#x2605;</li>
-					<li>Company</li>
-					<li>Product</li>
-					<li>Year</li>
-				</ul>
-				<p className="feature-description"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				<button className="more-features">Add To MyList</button>
-			</div>
+			<nav className="ad-display-nav group">
+				<button>Overview</button>
+				<button>More Like this</button>
+				<button>Details</button>
+			</nav>
 		);
 	},
 
@@ -25,12 +20,8 @@ var MainHeader = React.createClass({
 				<div className="bottom-grade"></div>
 				<div className="back-grade">
 					<div className="header-spacer header-left-arrow"/>
-					{ this.createOverview() }
-					<nav className="ad-display-nav group top">
-						<button>Overview</button>
-						<button>More Like this</button>
-						<button>Details</button>
-					</nav>
+					{ <OverviewDetail /> }
+					{ this.createMenuButtons() }
 					<button className="header-right-arrow header-spacer"></button>
 				</div>
 			</div>
