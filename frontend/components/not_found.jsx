781a0a2fe28b973359de2baa0ab6670b5ba004f0
Adam <ebokianknight@gmail.com>
2016-04-02 01:34:06 -0400

    404 Page Initialized

--- frontend/components/not_found.jsx
+++ frontend/components/not_found.jsx
@@ -2,10 +2,20 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 
 var NotFound = React.createClass({
+  contextTypes: { router: PropTypes.object.isRequired },
+  
+  home: function () {
+    this.context.router.push('/')
+  },
 
   render: function() {
     return (
-      <div>404 Page Not Found.</div>
+      <div className="not-found-body">
+        <src className="mammoth" onClick={this.home}/>
+        <p className="not-found-message test">404 Page Not Found</p>
+        <copy className="copyright">EbokianGames
+          © 2002-2016</copy>
+      </div>
     );
   }
 
