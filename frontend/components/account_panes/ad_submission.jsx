e7d8daa989c4e6ddcfd618efcab28ae7a1da926d
Adam <ebokianknight@gmail.com>
2016-04-02 01:37:47 -0400

    Placeholder for Logic

---
+++ frontend/components/account_panes/ad_submission.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var AddSubmission = React.createClass({
+
+  render: function() {
+    return (
+      <div className="account-pane group">
+        <aside className="account-side">
+          <strong>I'm a Form</strong>
+        </aside>
+      </div>
+    );
+  }
+
+});
+
+module.exports = AddSubmission;
