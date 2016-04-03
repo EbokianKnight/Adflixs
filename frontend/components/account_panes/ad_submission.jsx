0eb7a9b8022dca29e1cc387c6084b37cdb6f9d72
Adam <ebokianknight@gmail.com>
2016-04-03 12:52:56 -0400

    Refactored using better logic and Ref Data Packaging

--- frontend/components/account_panes/ad_submission.jsx
+++ frontend/components/account_panes/ad_submission.jsx
@@ -8,47 +8,14 @@ var AddSubmission = React.createClass({
 
   getInitialState: function() {
     return {
-      genres: GenreStore.all(),
-      success: false,
-      title: "",
-      product: "",
-      company: "",
-      year: 0,
-      description: "",
-      youtube: ""
+      genres: []
     };
-  },
-
-  updateTitle: function (e) {
-    if (this.state.success) {
-      this.setState({ title: e.target.value, success: false });
-    } else {
-      this.setState({ title: e.target.value });
-    }
-  },
-
-  updateProduct: function (e) {
-    this.setState({ product: e.target.value });
-  },
-  updateCompany: function (e) {
-    this.setState({ company: e.target.value });
-  },
-  updateYear: function (e) {
-    this.setState({ year: e.target.value });
-  },
-  updateDescription: function (e) {
-    this.setState({ description: e.target.value });
-  },
-  updateYouTube: function (e) {
-    this.setState({ youtube: e.target.value });
   },
 
   componentDidMount: function() {
     this.adToken = AdStore.addListener(this.adRecieved);
     this.genreToken = GenreStore.addListener(this.updateGenres);
-    if (this.state.genres.length === 0) {
-      ApiUtil.fetchGenres()
-    }
+    ApiUtil.fetchGenreList()
   },
 
   componentWillUnmount: function() {
@@ -57,28 +24,7 @@ var AddSubmission = React.createClass({
   },
 
   adRecieved: function () {
-    var success = AdStore.successMessage()
-    if (success) {
-      this.setState({
-        success: success,
-        title: "",
-        product: "",
-        company: "",
-        year: 0,
-        description: "",
-        youtube: ""
-      });
-    }
-  },
-
-  extractCheckedBoxes: function (nodeList) {
-    var checked = [];
-    for (var i = 0; i < nodeList.length; i++) {
-      if (nodeList[i].checked) {
-        checked.push( parseInt(nodeList[i].id) )
-      }
-    }
-    return checked;
+    this.props.close("success");
   },
 
   createCheckBoxes: function () {
@@ -87,7 +33,7 @@ var AddSubmission = React.createClass({
         return (
           <div className="account-checkbox" key={idx}>
             <input className="account-checkbox-item" type="checkbox"
-              name="genres" key={genre.id} id={genre.id}/>
+              name="ad[genre_ids][]" key={genre.id} value={genre.id}/>
             <strong>{genre.name}</strong>
           </div>
         );
@@ -96,27 +42,12 @@ var AddSubmission = React.createClass({
   },
 
   updateGenres: function() {
-    this.setState({ genres: GenreStore.all() })
-  },
-
-  sayMessage: function () {
-    if (this.state.success) {
-      return <div className="flash-message">SUCCESS!!</div>
-    }
+    this.setState({ genres: GenreStore.fetchGenreList() });
   },
 
   sendNewAdvertRequest: function (e) {
     e.preventDefault()
-    var form = e.currentTarget
-    ApiUtil.createAdvert({
-      description: form.description.value,
-      title: form.title.value,
-      year: form.year.value,
-      product: form.product.value,
-      company: form.company.value,
-      youtube: form.youtube.value,
-      genre_ids: this.extractCheckedBoxes(form.genres)
-    });
+    ApiUtil.createAdvert($(this.refs.AdvertRequest).serialize());
   },
 
   createAdForm: function () {
@@ -127,13 +58,12 @@ var AddSubmission = React.createClass({
           <button onClick={this.props.close} className="account-aside-button">
             Cancel</button>
         </section>
-        <form onSubmit={this.sendNewAdvertRequest}>
+        <form ref="AdvertRequest" onSubmit={this.sendNewAdvertRequest}>
           <row className="account-section-row group">
             <label className="account-item-left group">
               <div className="form-row">Title</div>
               <input className="account-section-input" type="text"
-                name="title" value={this.state.title}
-                onChange={this.updateTitle}/>
+                name="ad[title]"/>
             </label>
             <input type="submit" value="Submit Advert"
               className="account-aside-button account-item-right"/>
@@ -142,32 +72,28 @@ var AddSubmission = React.createClass({
             <label className="account-item-left group">
               <div className="form-row">Company</div>
               <input className="account-section-input" type="text"
-                name="company" value={this.state.company}
-                onChange={this.updateCompany}/>
+                name="ad[company]"/>
             </label>
           </row>
           <row className="account-section-row group">
             <label className="account-item-left group">
               <div className="form-row">Year</div>
-              <input className="account-section-input" type="date"
-                name="year" value={this.state.year}
-                onChange={this.updateYear}/>
+              <input className="account-section-input as-date" type="date"
+                name="ad[year]"/>
             </label>
           </row>
           <row className="account-section-row group">
             <label className="account-item-left group">
               <div className="form-row">Product</div>
               <input className="account-section-input" type="text"
-                name="product" value={this.state.Product}
-                onChange={this.updateProduct}/>
+                name="ad[product]"/>
             </label>
           </row>
           <row className="account-section-row group">
             <label className="account-item-left group">
               <div className="form-row">Youtube Link</div>
               <input className="account-section-input" type="text"
-                name="youtube" value={this.state.youtube}
-                onChange={this.updateYouTube}/>
+                name="ad[youtube]"/>
             </label>
           </row>
           <row className="account-section-row account-checkbox-container group">
@@ -177,8 +103,7 @@ var AddSubmission = React.createClass({
             <label className="account-item-left group">
               <div className="form-row">Description</div>
               <textarea className="account-section-input as-area" type="textarea"
-                name="description" value={this.state.description}
-                onChange={this.updateDescription}/>
+                name="ad[description]"/>
             </label>
           </row>
         </form>
