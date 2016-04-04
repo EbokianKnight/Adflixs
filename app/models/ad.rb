8c9911db8028fe6da2a501f6f95b582719d2498e
Adam <ebokianknight@gmail.com>
2016-04-04 14:57:06 -0400

    Added through validations for Ads and Users

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,4 +3,5 @@ class Ad < ActiveRecord::Base
 	has_many :ad_genres
 	has_many :genres, through: :ad_genres, source: :genre
 	has_many :views
+	has_many :viewed, through: :views, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 	attr_reader :password
 
 	has_many :views
+	has_many :viewed, through: :views, source: :ad
 
 	def self.generate_session_token
 		token = SecureRandom.urlsafe_base64(16)
