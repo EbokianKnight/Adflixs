5074afde095f29815dbf0d08e3c1afe1863f165c
Adam <ebokianknight@gmail.com>
2016-04-04 14:18:44 -0400

    Finished Controller View actions

---
+++ app/controllers/api/views_controller.rb
@@ -0,0 +1,32 @@
+class Api::ViewsController < ApplicationController
+
+  def show
+    @view = View.find(params[:id])
+  end
+
+  def create
+    @view = View.new(vew_params)
+    @view.user_id = current_user.id
+    if @view.save!
+      render :show
+    else
+      render :json { messages: @view.errors.full_messages }, status: 401
+    end
+  end
+
+  def update
+    @view = view.find(params[:id])
+    if @view.update_attributes(view_params)
+      render :show
+    else
+      render :json { messages: @view.errors.full_messages }, status: 401
+    end
+  end
+
+  private
+
+  def view_params
+    params.require(:view).permit(:ad_id, :title, :rate, :review)
+  end
+
+end
---
+++ spec/factories/views.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :view do
+    
+  end
+end
