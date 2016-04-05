424ee37c8d132abea821f4aa00039c5bf2144861
Adam <ebokianknight@gmail.com>
2016-04-04 20:28:26 -0400

    Added flux logic for View Update and Create

--- frontend/stores/session_store.js
+++ frontend/stores/session_store.js
@@ -2,11 +2,12 @@ var Store = require('flux/utils').Store;
 var UserConstants = require('../constants/user_constants');
 var AppDispatcher = require('../dispatcher/dispatcher');
 
-var SessionStore = new Store(AppDispatcher);
+SessionStore = new Store(AppDispatcher);
 
 var _currentUser;
 var _isLoggedIn = false;
 var _flashedMessage = null;
+var _myViews = {};
 
 SessionStore.currentUser = function () {
   return _currentUser;
@@ -19,6 +20,7 @@ SessionStore.isLoggedIn = function () {
 var setUser = function (payload) {
   _currentUser = payload.currentUser;
   _isLoggedIn = true;
+  storeCollections(payload.currentUser.views);
   if (payload.callback) { payload.callback(); }
 };
 
@@ -32,10 +34,33 @@ var flashMessage = function (message) {
   _flashedMessage = message;
 };
 
+var storeCollections = function (views) {
+  _myViews = {};
+  views.forEach(function(view){
+    _myViews[view.ad_id] = view;
+  });
+};
+
+var updateView = function (view) {
+  _myViews[view.ad_id] = view;
+};
+
 SessionStore.flashMessage = function () {
   var message = _flashedMessage;
   _flashedMessage = null;
   return message;
+};
+
+SessionStore.getViews = function () {
+  return jQuery.extend({}, _myViews);
+};
+
+SessionStore.fetchView = function (adID) {
+  if (_myViews[adID]) {
+    return jQuery.extend({}, _myViews[adID]);
+  } else {
+    return false;
+  }
 };
 
 SessionStore.__onDispatch = function (payload) {
@@ -50,6 +75,10 @@ SessionStore.__onDispatch = function (payload) {
       break;
     case UserConstants.FLASH:
       flashMessage(payload.message);
+      SessionStore.__emitChange();
+      break;
+    case UserConstants.UPDATE_VIEW:
+      updateView(payload.view);
       SessionStore.__emitChange();
       break;
   }
