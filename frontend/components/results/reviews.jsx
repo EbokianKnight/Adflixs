714cc3680544555a8b62efbc784ddd4e03010db8
Adam <ebokianknight@gmail.com>
2016-04-05 14:50:32 -0400

    Added React-modal and setup reviews route for later searches

---
+++ frontend/components/results/reviews.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var ReviewResults = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        This will Render Search Components
+      </div>
+    );
+  }
+
+});
+
+module.exports = ReviewResults;
