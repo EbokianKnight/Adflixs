93aa9efc78f0a7979ef34013777f478d40b2bc79
Adam <ebokianknight@gmail.com>
2016-04-06 10:18:20 -0400

    Added Pagination Logic into the GenreController

--- frontend/stores/genre_store.js
+++ frontend/stores/genre_store.js
@@ -6,21 +6,29 @@ var GenreStore = new Store(AppDispatcher);
 
 var _genres = {};
 var _list = {};
+var page = 0;
+var last_page;
 
-var resetGenres = function (genres) {
-	_genres = {};
+var loadGenrePage = function (genres) {
 	genres.forEach( function (el) { _genres[el.id] = el; });
+	page++;
+	last_page = genres[0].pages;
 };
-
 var resetGenreList = function (list) {
 	_list = {};
 	list.forEach( function (el) { _list[el.id] = el; });
 };
 
+//asyncCallback for MainIndex Scroller
+var startTimeout = function (fn) {
+	if (fn) { fn(); }
+};
+
 GenreStore.__onDispatch = function (payload) {
 	switch (payload.actionType) {
 		case GenresConstants.GENRES_RECEIVED:
-			resetGenres(payload.genres);
+			loadGenrePage(payload.genres);
+			startTimeout(payload.callback);
 			GenreStore.__emitChange();
 		break;
 		case GenresConstants.GENRE_LIST:
@@ -29,6 +37,15 @@ GenreStore.__onDispatch = function (payload) {
 			break;
 	}
 };
+
+GenreStore.getCurrentPage = function () {
+	return page;
+};
+
+GenreStore.getLastPage = function () {
+	return last_page;
+};
+
 
 GenreStore.all = function () {
 	var genres = [];
