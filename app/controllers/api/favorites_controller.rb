c9c508fb54d136732a68239793882685224cdaae
Adam <ebokianknight@gmail.com>
2016-04-06 10:49:12 -0400

    Routes and Controller done for Favorites

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,2 +1,19 @@
 class Api::FavoritesController < ApplicationController
+
+  def create
+    @favorite = Favorite.new(params[:ad_id])
+    @favorite.user_id = current_user.id
+    if @favorite.save!
+      render :create
+    else
+      render json: { messages: @favorite.errors.full_messages }, status: 401
+    end
+  end
+
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy!
+    render json: {}
+  end
+
 end
