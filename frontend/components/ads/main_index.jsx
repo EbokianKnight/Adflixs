02c83443a0f1d437964930a03bddb8838343c486
Adam <ebokianknight@gmail.com>
2016-04-06 17:08:29 -0400

    Added helpful comments for confusing code, packages mylsit to be read correctly by rows components

--- frontend/components/ads/main_index.jsx
+++ frontend/components/ads/main_index.jsx
@@ -17,12 +17,14 @@ var MainIndex = React.createClass({
 
 	handleScroll: function (e) {
 		if (this.waiting) return;
+
 		if ((window.innerHeight + window.scrollY + 10) >= document.body.offsetHeight) {
 			this.getMoreRows();
 			this.waiting = true;
 		}
   },
 
+	//throttles Scroll Event
 	timeoutCallback: function () {
 		setTimeout(function(){ this.waiting = false; }.bind(this), 500);
 	},
@@ -30,11 +32,11 @@ var MainIndex = React.createClass({
 	componentDidMount: function() {
 		this.waiting = false;
 		$(window).bind('mousewheel', this.handleScroll);
+
 		this.genreStoreToken = GenreStore.addListener(this.getGenresFromStore);
 		this.myListStoreToken = MyListStore.addListener(this.getMyListFromStore);
-		if (this.state.genres.length === 0){
-			this.getMoreRows();
-		}
+
+		if (this.state.genres.length === 0){ this.getMoreRows(); }
 		ApiUtil.fetchMyList();
 	},
 
@@ -61,18 +63,26 @@ var MainIndex = React.createClass({
 		this.setState({ myList: MyListStore.all() });
 	},
 
+	// Renders the Individual Rows
 	fetchRows: function () {
 		return this.state.genres.map(function(row){
 			return <AdvertRow key={row.id} genre={row} />;
 		});
 	},
 
+	// Renders My List
 	fetchMyList: function () {
+		myListPackage = {
+			name: "My List",
+			id: "My List",
+			ads: this.state.myList
+		};
 		return (
-			<AdvertRow genre={"My List"} ads={this.state.myList}/>
+			<AdvertRow genre={myListPackage}/>
 		);
 	},
 
+	// Feature Faker placeholder
 	fetchRandomAd: function () {
 		var ads = this.state.genres[Math.floor(Math.random() * this.state.genres.length)].ads;
 		return ads[Math.floor(Math.random() * ads.length)];
