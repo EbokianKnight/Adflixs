e6163e797aa6f0fcb09793f62ddf746a4982a177
Adam <ebokianknight@gmail.com>
2016-04-06 10:24:27 -0400

    MyList Components Laid out

---
+++ frontend/components/ads/my_list/my_button.jsx
@@ -0,0 +1,16 @@
+// this is the button that toggles ads listed on my list
+
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var MyListButton = React.createClass({
+
+  render: function() {
+    return (
+      <div />
+    );
+  }
+
+});
+
+module.exports = MyListButton;
