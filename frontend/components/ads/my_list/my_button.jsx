c8e7f2de5f83fb6fa2303aafe48ae08196c4ef1a
Adam <ebokianknight@gmail.com>
2016-04-06 13:49:12 -0400

    Added My List Button component in render stream

--- frontend/components/ads/my_list/my_button.jsx
+++ frontend/components/ads/my_list/my_button.jsx
@@ -1,13 +1,62 @@
-// this is the button that toggles ads listed on my list
-
 var React = require('react');
 var PropTypes = React.PropTypes;
+var MyListStore = require('../../../stores/my_list_store.js');
+var ApiUtil = require('../../../util/api_util');
+// this is the button that toggles ads listed on my list
+// it requires an ad id to function
 
 var MyListButton = React.createClass({
+  getInitialState: function() {
+    var state;
+    if (MyListStore.find(this.props.ad.id)) {
+      state = "removeFrom";
+    } else {
+      state = "addTo";
+    }
+    return {
+      show: state,
+      hover: "off"
+    };
+  },
+
+  toggleList: function () {
+    if (this.state.show === "addTo") {
+      ApiUtil.addToMyList(this.props.ad, this.toggleState);
+    } else {
+      ApiUtil.removeFromMyList(this.props.ad, this.toggleState);
+    }
+  },
+
+  toggleState: function () {
+    if (this.state.show === "addTo") {
+      this.setState({ show: "removeFrom" });
+    } else {
+      this.setState({ show: "addTo" });
+    }
+  },
+
+  onHover: function () {
+    this.setState({ hover: "on" });
+  },
+
+  offHover: function () {
+    this.setState({ hover: "off" });
+  },
 
   render: function() {
+    var value, klass ;
+    if (this.state.hover === "off") {
+      value = this.state.show === "addTo" ? " +" : " ✓";
+      klass = " my-list-bubble";
+    } else {
+      value = this.state.show === "addTo" ? "Add To My List" : "Remove From My List";
+      klass = "";
+    }
     return (
-      <div />
+      <button className={"feature-my-list" + klass}
+        onMouseEnter={this.onHover}
+        onMouseLeave={this.offHover}
+        onClick={this.toggleList}>{value}</button>
     );
   }
 
