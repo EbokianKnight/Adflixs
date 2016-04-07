8167209ea04e26cd7112343ed816a08d222fd20f
Adam <ebokianknight@gmail.com>
2016-04-07 14:00:43 -0400

    Removed excessive validation

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,7 +1,6 @@
 class Ad < ActiveRecord::Base
 	has_attached_file :image, styles: { large: "1300x500^", thumb: "300x200>" }, default_url: "/images/:style/demo.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-	validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
 
 	validates :company, :product, :description, :year, :title, presence: true
 	has_many :ad_genres
