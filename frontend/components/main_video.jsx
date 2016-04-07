c8251fd0279b8aa66d0fc57c94219c5f8b8999ab
Adam <ebokianknight@gmail.com>
2016-04-06 20:33:35 -0400

    Movie Screen Resizes

--- frontend/components/main_video.jsx
+++ frontend/components/main_video.jsx
@@ -3,17 +3,32 @@ var PropTypes = React.PropTypes;
 
 var MainVideo = React.createClass({
 
-  componentDidMount: function() {
+  getInitialState: function() {
+    return {
+      windowWidth: window.innerWidth
+    };
+  },
 
+  componentDidMount: function() {
+    window.addEventListener('resize', this.handleResize);
+  },
+
+  componentWillUnmount: function() {
+    window.removeEventListener('resize', this.handleResize);
+  },
+  handleResize: function(e) {
+    this.setState({windowWidth: window.innerWidth});
   },
 
   render: function() {
     var youtubeLink = this.props.location.query.youtube;
+    var width = this.state.windowWidth * 0.8;
+    var height = width * 0.64;
     return (
       <div className="video-main-container">
         <iframe
-          width="1020"
-          height="640"
+          width={ width }
+          height={ height }
           src={"https://www.youtube.com/embed/" + youtubeLink}
           frameBorder="0" allowFullScreen></iframe>
       </div>
