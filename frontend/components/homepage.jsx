b546eec668152005b2d0c13e50812dcbedeb15bd
Adam <ebokianknight@gmail.com>
2016-04-07 14:04:07 -0400

    Added react slick and strange hacky bugfix for autoplay. Will look into auto play bug over the weekend

--- frontend/components/homepage.jsx
+++ frontend/components/homepage.jsx
@@ -1,8 +1,20 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var Slider = require('react-slick');
 
 var HomePage = React.createClass({
 	contextTypes: { router: PropTypes.object.isRequired },
+
+	componentDidMount: function() {
+    document.body.style.backgroundColor = "#141414";
+  },
+
+	componentWillUnmount: function() {
+		var interval_id = window.setInterval("", 9999);
+		for (var i = 1; i < interval_id; i++) {
+			window.clearInterval(i);
+		}
+	},
 
 	goToSignUp: function () {
 		this.context.router.push("/users/new");
@@ -13,9 +25,10 @@ var HomePage = React.createClass({
 	},
 
 	generateHomeHeader: function () {
-		return (
-			<div>
-				<header className="homepage-header" >
+		var slide = ["pepsi-header", "nuka-header", "liquor-header", "rabbit-header"];
+		return slide.map(function(klass, idx){
+			return (
+				<header key={idx}className={"homepage-header " + klass} >
 					<src className="logo"/>
 					<div className="home-wrapper">
 						<content className="home-text">
@@ -23,21 +36,32 @@ var HomePage = React.createClass({
 							<p className="home-pricing">WATCH ANYWHERE. CANCEL ANYTIME.</p>
 						</content>
 						<button className="home-sign-up home-button"
-							onClick={this.goToSignUp}>JOIN FREE FOR ONE MONTH
+							onClick={this.goToSignUp}>JOIN FREE FOR ALL MONTHS!
 						</button>
 					</div>
 					<button className="home-sign-in home-button"
 						onClick={this.goToSignIn}>Sign In
 					</button>
 				</header>
-			</div>
-		);
+			)
+		}.bind(this));
 	},
 
 	render: function() {
+		var settings = {
+      dots: false,
+			arrows: false,
+      speed: 1000,
+			autoplay: true,
+			autoplaySpeed: 6000,
+			fade: true,
+			useCSS: true
+    };
 		return (
 			<div id="basicLayout">
-				{ this.generateHomeHeader() }
+				<Slider {...settings}>
+					{ this.generateHomeHeader() }
+				</Slider>
 			</div>
 		);
 	}
