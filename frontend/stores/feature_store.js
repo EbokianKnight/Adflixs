6f598d265616c38d20862324f7fd1c66d372ad14
Adam <ebokianknight@gmail.com>
2016-04-28 10:41:49 -0400

    Store now listens for ad updates and emits change if the update would update a feature

--- frontend/stores/feature_store.js
+++ frontend/stores/feature_store.js
@@ -1,6 +1,7 @@
 var AppDispatcher = require('../dispatcher/dispatcher');
 var Store = require('flux/utils').Store;
 var FeatureConstants = require('../constants/feature_constants');
+var AdConstants = require('../constants/ad_constants');
 
 var FeatureStore = new Store(AppDispatcher);
 
@@ -10,12 +11,30 @@ var resetFeatures = function (ads) {
 	_features = ads;
 };
 
+var updateFeature = function (adUpdate) {
+	var toUpdate = false;
+	_features = _features.map(function(ad){
+		if (ad.id === adUpdate.id) {
+			toUpdate = true;
+			return adUpdate;
+		} else {
+			return ad;
+		}
+	});
+	if (toUpdate) {
+		FeatureStore.__emitChange();
+	}
+};
+
 FeatureStore.__onDispatch = function (payload) {
 	switch (payload.actionType) {
 		case FeatureConstants.FEATURES_RECEIVED:
 			resetFeatures(payload.ads);
 			FeatureStore.__emitChange();
-		break;
+			break;
+		case AdConstants.AD_RECEIVED:
+			updateFeature(payload.advert);
+			break;
 	}
 };
 
