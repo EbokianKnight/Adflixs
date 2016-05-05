ff040cc9f6f9e57a706a13eb5ef55a478afd01a0
Adam <ebokianknight@gmail.com>
2016-05-05 02:04:55 -0400

    Removed unused and distracting functions

--- frontend/actions/ad_actions.js
+++ frontend/actions/ad_actions.js
@@ -2,13 +2,6 @@ var Dispatcher = require('../dispatcher/dispatcher.js');
 var AdConstants = require('../constants/ad_constants.js');
 
 module.exports = {
-  receiveAllAdverts: function (adverts) {
-    Dispatcher.dispatch({
-      actionType: AdConstants.ADS_RECEIVED,
-      adverts: adverts
-    });
-  },
-
 	receiveAdvert: function (advert) {
     Dispatcher.dispatch({
       actionType: AdConstants.AD_RECEIVED,
