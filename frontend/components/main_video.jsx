a7ca793f183eb5188de877bd41738cabe9837dc8
Adam <ebokianknight@gmail.com>
2016-05-05 11:25:53 -0400

    Added the Controller to the Video

--- frontend/components/main_video.jsx
+++ frontend/components/main_video.jsx
@@ -1,9 +1,7 @@
 var React = require('react'),
     PropTypes = React.PropTypes,
     ApiUtil = require('../util/api_util'),
-    YouTubePlayer = require('youtube-player'),
     YoutubeUtil = require('../util/youtube_util'),
-    ReactCSS = require('react-addons-css-transition-group'),
     AdStore = require('../stores/ad_store');
 
 var VideoModel = React.createClass({
@@ -14,130 +12,186 @@ var VideoModel = React.createClass({
       playing: false,
       controls: false,
       progress: 0,
-      seconds: 0
+      volume: 0,
+      mute: 0,
+      duration: 0,
     };
   },
 
   componentDidMount: function() {
+    this.player;
     YoutubeUtil.loadApiScript();
-    window.addEventListener("keydown", this.mapKeyHandlers);
     this.renderPlayer();
+    this.loadingScreen();
   },
 
   componentWillUnmount: function () {
-    window.removeEventListener("keydown", this.mapKeyHandlers);
+    window.clearInterval(this.loadtime);
     this.cancelHide();
-  },
-
-  mapKeyHandlers: function (e) {
-    e.stopPropagation();
-    if (e.keyCode === 32) this.togglePlay();
-  },
-
-  returnToIndex: function () {
-    this.context.router.push("/main");
-  },
-
-  togglePlay: function () {
-    if (!this.state.playing) {
-      this.player.playVideo();
-      this.setState({ playing: true });
-    } else {
-      this.player.pauseVideo();
-      this.setState({ playing: false });
-    }
-  },
-
-  showControls: function () {
-    this.setState({ controls: true });
-    this.cancelHide();
-  },
-
-  hideControls: function () {
-    this.hideTimer = window.setTimeout(this.hideControlsNow, 100000);
   },
 
   cancelHide: function () {
     clearInterval(this.hideTimer);
   },
 
+  hideControls: function () {
+    this.hideTimer = window.setTimeout(this.hideControlsNow, 1200);
+  },
+
   hideControlsNow: function () {
     this.setState({ controls: false });
   },
 
+  loadingScreen: function () {
+    this.loadtime = window.setInterval(this.loadingScreenFn, 200);
+  },
+
+  loadingScreenFn: function () {
+    this.forceUpdate();
+    console.log("check");
+  },
+
+  onPlayerLoad: function () {
+    this.togglePlay();
+    var time = this.player.getDuration();
+    this.setState({
+      volume: this.player.getVolume(),
+      duration: Math.floor(time/60)+"m "+ Math.floor(time%60)+"s",
+      mute: this.player.isMuted(),
+    })
+  },
+
+  requestFullScreen: function () {
+    var iframe = this.player.c
+    var requestFullScreen = iframe.requestFullScreen || iframe.mozRequestFullScreen || iframe.webkitRequestFullScreen;
+    if (requestFullScreen) {
+      requestFullScreen.bind(iframe)();
+    } else {
+      debugger;
+    }
+  },
+
+  renderBackButton: function(seen) {
+    return <div className={"movie-back " + seen}
+      onClick={this.returnToIndex}/>;
+  },
+
+  renderControls: function (seen) {
+    var play = this.state.playing ? " video-pause" : " video-play"
+    var volume;
+    if (this.state.volume === 0 || this.state.mute) {
+      volume = "mute"
+    } else if (this.state.volume <= 30) {
+      volume = "low"
+    } else if (this.state.volume <= 60) {
+      volume = "med"
+    } else {
+      volume = "high"
+    }
+    return (
+      <div className={"video-control-container " + seen}>
+        <div className="video-flex-row">
+          <span className="progress-bar"></span>
+          <p>{this.state.duration}</p>
+        </div>
+        <menu className="video-controller video-flex-row">
+          <div className="video-flex-row">
+            <button className={"video-icon" + play} onClick={this.togglePlay}/>
+            <div className="video-spacer"/>
+            <button className={"video-icon video-volume-"+volume} onClick={this.toggleSound}/>
+            <div className="video-spacer"/>
+            <p className="video-title">
+              {this.props.location.query.title}</p>
+          </div>
+          <div className="video-flex-row">
+            <div className="video-spacer"/>
+            <button className="video-icon video-full-screen"
+              onClick={this.requestFullScreen}/>
+          </div>
+        </menu>
+      </div>
+    );
+  },
+
   renderPlayer: function() {
-    var url = this.props.location.query.youtube;;
-    this.player = YoutubeUtil.loadPlayer(url, this.videoStateChange, this.togglePlay);
+    if (!this.player) {
+      var url = this.props.location.query.youtube;
+      this.player = YoutubeUtil.loadPlayer(url, this.videoStateChange, this.onPlayerLoad);
+      if (this.player) {
+        window.clearInterval(this.loadtime);
+      }
+    }
   },
 
-  videoStateChange: function () {
-    if (!this.player) return;
-    if (this.player.D.playerState === 0) {
+  returnToIndex: function () {
+    this.context.router.push("/main");
+  },
+
+  showControls: function () {
+    if (!this.state.controls) {
+      this.setState({ controls: true });
+    }
+    this.cancelHide();
+  },
+
+  togglePlay: function () {
+    if (!this.state.playing) {
+      this.player.playVideo();
+    } else {
+      this.player.pauseVideo();
+    }
+  },
+
+  toggleSound: function () {
+    if (this.state.mute) {
+      this.player.unMute();
+      this.setState({ mute: false });
+    } else {
+      this.player.mute();
+      this.setState({ mute: true });
+    }
+  },
+
+  videoStateChange: function (e) {
+    if (!e) return;
+    if (e.data === 0) {
       this.returnToIndex();
+    } else if (e.data === 1) {
+      this.setState({ playing: true, controls: true});
+      this.hideControls();
+    } else if (e.data === 2) {
+      this.setState({ playing: false, controls: true});
+      this.hideControls();
+    } else if (e.data === -1) {
+      this.setState({});
     }
-  },
-
-  renderBackButton: function() {
-    if (this.state.controls) {
-      return <div
-        className="movie-back"
-        onClick={this.returnToIndex}/>;
-    }
-  },
-
-  renderControls: function () {
-    // if (this.state.controls) {
-    //   var play = this.state.playing ? " video-pause" : " video-play"
-    //   return (
-    //     <container>
-    //       <div className="video-flex-row">
-    //         <span className="progress-bar"></span>
-    //         <p>{this.state.seconds}</p>
-    //       </div>
-    //       <menu className="video-controller">
-    //         <div className="video-flex-row">
-    //           <div>
-    //             <button className={"video-icon" + play} onClick={this.togglePlay}/>
-    //             <button className="video-icon video-volumn"/>
-    //             <p className="video-title">
-    //               {this.props.location.query.title}</p>
-    //           </div>
-    //           <div>
-    //             <button className="video-icon video-full-screen"/>
-    //           </div>
-    //         </div>
-    //       </menu>
-    //     </container>
-    //   )
-    // }
   },
 
   render: function() {
+    var seen = this.state.controls ? "video-show" : "video-hide"
+    var loading = this.state.duration > 0 ? "loader" : "";
     return (
       <div className="modal-layer">
-        <container id="video-player">{this.renderPlayer()}</container>
+        <container id="video-player">
+          {this.renderPlayer()}
+          <div className={loading}/>
+        </container>
         <div className={"movie-leftside"}
           onMouseEnter={this.showControls}
           onMouseLeave={this.hideControls}>
-          <ReactCSS transitionName="fader"
-  					transitionEnterTimeout={500}
-  					transitionLeaveTimeout={500}>
-            { this.renderBackButton() }
-          </ReactCSS>
+          { this.renderBackButton(seen) }
         </div>
         <div className={"movie-bottom"}
           onMouseEnter={this.showControls}
           onMouseLeave={this.hideControls}>
-          <ReactCSS transitionName="fader"
-  					transitionEnterTimeout={500}
-  					transitionLeaveTimeout={500}>
-            { this.renderControls() }
-          </ReactCSS>
+          { this.renderControls(seen) }
         </div>
       </div>
     );
   }
 
 });
+
+
 
 module.exports = VideoModel;
