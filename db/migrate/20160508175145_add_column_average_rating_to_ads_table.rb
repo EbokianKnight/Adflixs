2ae4168ae9fffc8a3d5c10b7380c9b2e73e72baf
Adam <ebokianknight@gmail.com>
2016-05-08 19:16:09 -0400

    loaded seeds for last migration

--- db/migrate/20160508175145_add_column_average_rating_to_ads_table.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnAverageRatingToAdsTable < ActiveRecord::Migration
-  def change
-
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -297,3 +297,12 @@ end
       )
   end
 end
+
+Ad.all.each do |ad|
+  if (ad.views.average(:rate))
+    average = ad.views.average(:rate).round
+  else
+    average = 0
+  end
+  ad.update_attributes(average_rate: average)
+end
